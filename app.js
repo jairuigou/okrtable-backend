1812e707b1a2d3021eab6ae88ce205428fc098f3
jairuigou <jairuigou@gmail.com>
2021-11-12 12:00:38 +0800

    Feat: new api and middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mariadb = require('mariadb');
 const express = require('express');
+const {checkRequest} = require('./middleware');
 
 const app = express();
 const port = 3000;
@@ -22,13 +23,9 @@ mariadb.createConnection({
     // select all object whose ddl date in range.
     // range from yyyy-mm-dd 00:00:00 to n*24 hours later
     // n must >= 0 , if n == 0 , only select the object whose ddl is at yyyy-mm-dd 00:00:00
-    app.post('/',(req,res)=>{
+    app.post('/',checkRequest,(req,res)=>{
         if( !("level" in req.body) ){
             res.json({error:"no level parameter"});
-            return;
-        }
-        if( req.body.level != 0 && req.body.level != 1){
-            res.json({error:"level parameter can only be 0 or 1"});
             return;
         }
         if( !("start" in req.body) ){
@@ -39,43 +36,199 @@ mariadb.createConnection({
             res.json({error:"no duration parameter"});
             return;
         } 
-        if( req.body.duration < 0){
-            res.json({error:"duration parameter cannot smaller than 0"});
-            return;
-        }
         
-        var startDate = new Date(req.body.start);
-        if( startDate == "Invalid Date"){
-            res.json({error:"incorrect start date format"});
-            return;
-        }
-        var endDate = new Date(req.body.start);
-        endDate.setDate(startDate.getDate() + req.body.duration);
-        var startDateStr = startDate.getFullYear().toString().padStart(4,"0") + "-" + 
-                            (startDate.getMonth()+1).toString().padStart(2,"0") + "-" +
-                                startDate.getDate().toString().padStart(2,"0");
-        var endDateStr = endDate.getFullYear().toString().padStart(4,"0") + "-" + 
-                            (endDate.getMonth()+1).toString().padStart(2,"0") + "-" +
-                                endDate.getDate().toString().padStart(2,"0");
+        var startDate = new Date(req.body.start + " 00:00:00");
+        var endDate = new Date(req.body.start + " 00:00:00");
+        endDate.setDate(startDate.getDate() + parseInt(req.body.duration));
+        var startDateStr = date2str(startDate);  
+        var endDateStr = date2str(endDate);
 
-        querysql = "select * from info where ddl >= \'" + 
+        var queryRange = "select * from info where ddl >= \'" + 
                     startDateStr + "\' and ddl <= \'" + endDateStr + "\' and level = " + req.body.level;
-
-        console.log("querysql: "+ querysql);
-
-        conn.query(querysql)
-            .then(rows=>{
-                console.log(rows);
+        
+        conn.query({dateStrings:true, sql:queryRange})
+            .then( (rows)=>{
                 res.send(rows);
             })
             .catch(err=>{
                 console.log("Error: " + err);
+                res.json({error:"query error"});
             })
+    });
+
+    // create new record
+    // detail: String
+    // level: 0 or 1
+    // priority: 0,1,2
+    // state: "PENDING","INPROG","DONE","BLOCKED","KILLED"
+    // ddl: yyyy-mm-dd hh:mm:ss
+    app.post('/create',checkRequest,(req,res)=>{
+        // todo check detail content for security
+        if( !("detail" in req.body) ){
+            res.json({error:"no detail parameter"});
+            return;
+        }
+        var detail = req.body.detail;
+
+        if( !("level" in req.body) ){
+            res.json({error:"no level parameter"});
+            return;
+        }
+        var level = req.body.level;
+
+        if( "priority" in req.body ){
+            var priority = req.body.priority;
+        }
+        else{
+            var priority = 2;
+        }
+
+        if( "state" in req.body ){
+            var state = req.body.state;
+        }
+        else{
+            var state = "PENDING";
+        }
+
+        if( !("ddl" in req.body) ){
+            res.json({error:"no ddl parameter"});
+            return;
+        }
+        var ddl = req.body.ddl;
+
+        var today = new Date(Date.now());
+        var start = today.getFullYear() * 10000 + (today.getMonth()+1)*100 + today.getDate();
+        start *= 100;
+        var end = start + 99;
+        var queryCount = "select count(*) as count from info where id >= " + start + " and id <= " + end;
+        var id = start;
+
+        conn.query(queryCount)
+            .then(rows=>{
+                id = start + rows[0].count;
+                var queryInsert = "insert into info values (" + id + ",\'" + detail + "\'," + level
+                                    + "," + priority + ",\'" + state + "\',\'" + ddl + "\')";
+                return conn.query(queryInsert);    
+            })
+            .then(rows=>{
+                var createDate = date2str(new Date(Date.now()));
+                var queryInsert = "insert into ddl values (" + id + ",\'" + ddl + "\',\'" + createDate + "\')";
+                console.log(queryInsert);
+                return conn.query(queryInsert); 
+            })
+            .then(rows=>{
+                res.json({success:id});
+            })
+            .catch(err=>{
+                console.log("Error: " + err);
+                res.json({error: err});
+            })
+    });
+
+    // update priority
+    // id: yyyymmddnn
+    // priority: 0,1,2
+    // todo check query result
+    app.post('/updateprior',checkRequest,(req,res)=>{
+        if( !("id" in req.body) ){
+            res.json({error:"no id parameter"});
+            return;
+        }
+        if( !("priority" in req.body) ){
+            res.json({error:"no priority parameter"});
+            return;
+        }
+        
+        var queryUpdate = "update info set priority = " + req.body.priority
+                            + " where id = " + req.body.id;
+        
+        conn.query(queryUpdate)
+            .then(rows=>{
+                res.json({success:"success"});
+            })
+            .catch(err=>{
+                console.log(err);
+                res.json({error:err});
+            })
+    });
+
+    // todo use middleware to check date format
+    // update state
+    // id: yyyymmddnn
+    // state: "PENDING","INPROG","DONE","BLOCKED","KILLED"
+    app.post('/updatestate',checkRequest,(req,res)=>{
+        if( !("id" in req.body) ){
+            res.json({error:"no id parameter"});
+            return;
+        }    
+        if( !("state" in req.body) ){
+            res.json({error:"no state parameter"});
+            return;
+        }
+
+        var queryUpdate = "update info set state = " + req.body.state
+                            + " where id = " + req.body.id;
+        
+        conn.query(queryUpdate)
+            .then(rows=>{
+                res.json({success:"success"});
+            })
+            .catch(err=>{
+                console.log(err);
+                res.json({error:err});
+            })
+    });
+
+    // update ddl
+    // id: yyyymmddnn
+    // ddl: 'yyyy-mm-dd hh:mm:ss'
+    // todo compare ddl with current time and change state
+    app.post('/updateddl',checkRequest,(req,res)=>{
+        if( !("id" in req.body) ){
+            res.json({error:"no id parameter"});
+            return;
+        }
+        if( !("ddl" in req.body) ){
+            res.json({error:"no ddl parameter"});
+            return;
+        }
+        var id = req.body.id;
+        var ddl = req.body.ddl;
+
+        var queryUpdate = "update info set ddl = \'" + ddl + "\' where id = " + id;
+
+        conn.query(queryUpdate)
+        .then(rows=>{
+            var createDate = date2str(new Date(Date.now())) 
+            var queryInsert = "insert into ddl values (" + id + ",\'" + ddl + "\',\'" +
+                                createDate + "\' )";
+            return conn.query(queryInsert);
+        })
+        .then(rows=>{
+            res.json({success:"success"});
+        })
+        .catch(err=>{
+            res.json({error:err});
+        })
+        
+    });
+
+    // update progress
+    app.post('/updateprogress',checkRequest,(req,res)=>{
+
     });
 
     app.listen(port,()=>{console.log("listening at http://localhost:${port}")});
 })
 .catch(err=>{
-    console.log("connect error");
+    console.log(err);
 });
 
+function date2str(date){
+    return date.getFullYear().toString().padStart(4,"0") + "-" + 
+            (date.getMonth()+1).toString().padStart(2,"0") + "-" +
+              date.getDate().toString().padStart(2,"0") + " " + 
+                date.getHours().toString().padStart(2,"0") + ":" +
+                  date.getMinutes().toString().padStart(2,"0") + ":" +
+                    date.getSeconds().toString().padStart(2,"0"); 
+}
---
+++ middleware.js
@@ -0,0 +1,51 @@
+function checkRequest(req,res,next){
+    if( "id" in req.body ){
+        var re = new RegExp(/^\d{10}$/);
+        if( !(/^\d{10}$/).test(req.body.id) ){
+            res.json({error:"id format error : YYYYMMDDNN"});
+            return;
+        }
+    }
+    if( "level" in req.body ){
+        if( req.body.level != 0 && req.body.level != 1){
+            res.json({error:"level parameter can only be 0 or 1"});
+            return;
+        }
+    }
+    if( "priority" in req.body ){
+        if( req.body.priority != 0 && req.body.priority != 1 && req.body.priority != 2){
+            res.json({error:"priority parameter can only be 0,1,2"});
+            return;
+        }
+    }
+    if( "state" in req.body ){
+        if( req.body.state != "PENDING" && req.body.state != "INPROG" && req.body.state != "DONE"
+                && req.body.state != "BLOCKED" && req.body.state != "KILLED" ){
+                    res.json({error:"state parameter can only be PENDING,INPROG,DONE,BLOCKED,KILLED"});
+                    return;
+        }
+    }
+    if( "ddl" in req.body ){
+        if( !(/^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}$/).test(req.body.ddl) ){
+            res.json({error:"ddl format error : YYYY-MM-DD hh:mm:ss"});
+            return;
+        }
+    }
+    if( "duration" in req.body ){
+        if( req.body.duration < 0 ){
+            res.json({error:"duration parameter cannot smaller than 0"});
+            return;
+        }
+    }
+    if( "start" in req.body){
+        if( !(/^\d{4}-\d{2}-\d{2}$/).test(req.body.start)){
+            res.json({error:"start format error : YYYY-MM-DD"});
+            return;
+        }
+    }
+    
+    next();
+}
+
+
+module.exports.checkRequest = checkRequest;
