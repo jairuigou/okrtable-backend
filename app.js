604f24d2bedd5f1a33a5eea9672a910dce3424de
jairui <jairuigou@gmail.com>
2022-01-24 22:20:10 +0800

    Add scheduled task and fix bug

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const express = require('express');
 const cors = require('cors');
 const {checkRequest} = require('./middleware');
-const {date2str} = require('./utils');
+const {date2str,initJobs,isActivated,updateAutoDelayJob, updateNotifyJob} = require('./utils');
 
 const app = express();
 const port = 3000;
@@ -23,6 +23,12 @@ app.use(cors());
     catch(err){
         console.log("connect to db error",err);
     }
+
+    var notifyJobs = {};
+    var autoDelayJobs = {};
+    var param = { conn:conn,notifyJobs:notifyJobs,autoDelayJobs:autoDelayJobs };
+
+    await initJobs(param);
     
     // start : yyyy-mm-dd
     // duration : n days
@@ -146,6 +152,9 @@ app.use(cors());
                 return conn.query(queryInsert); 
             })
             .then(rows=>{
+                var info = {id:id,detail:detail,level:level,priority:priority,state:state,ddl:ddl};
+                updateNotifyJob(info,param);
+                updateAutoDelayJob(info,param); 
                 res.json({success:id});
             })
             .catch(err=>{
@@ -167,18 +176,33 @@ app.use(cors());
             res.json({error:"no priority parameter"});
             return;
         }
-        
-        var queryUpdate = "update info set priority = " + req.body.priority
+        var id = req.body.id;
+        var sql = "select * from info where id = " + id;
+        conn.query(sql)
+        .then(rows=>{
+            if( rows.length < 1 ){
+                return {error:"invalid id " + id};
+            }
+            if( !isActivated(rows[0].state) ){
+                return {error:"target " + id + " state is " + rows[0].state};
+            }
+
+            sql = "update info set priority = " + req.body.priority
                             + " where id = " + req.body.id;
-        
-        conn.query(queryUpdate)
-            .then(rows=>{
+            return conn.query(sql);
+        })
+        .then(rows=>{
+            if( 'error' in rows){
+                res.json(rows);
+            }
+            else{
                 res.json({success:"success"});
-            })
-            .catch(err=>{
-                console.log(err);
-                res.json({error:err});
-            })
+            }
+        })          
+        .catch(err=>{
+            console.log("updateprior:",err);
+            res.json({error:"unknown error"});
+        });
     });
         
     // todo use middleware to check date format
@@ -195,17 +219,38 @@ app.use(cors());
             return;
         }
 
-        var queryUpdate = "update info set state = \'" + req.body.state 
+        var sql = "select * from info where id = " + req.body.id;
+        conn.query(sql)
+        .then(rows=>{
+            if( rows.length < 1 ){
+                res.json({error:"invalid id "+id});
+                return;
+            }
+            if( !isActivated(rows[0].state) ){
+                res.json({error:"target " + id + " state is " + rows[0].state});
+                return;
+            }
+            var state = req.body.state;
+            if( state == 'KILLED' || state == 'DONE' || state == 'BLOCKED' ){
+                rows[0].state = state;
+                updateNotifyJob(rows[0],param);
+                updateAutoDelayJob(rows[0],param);
+                sql = "update info set state = \'" + req.body.state + "\', ddl=\'" + date2str(new Date(Date.now()))
                             + "\' where id = " + req.body.id;
-        
-        conn.query(queryUpdate)
-            .then(rows=>{
-                res.json({success:"success"});
-            })
-            .catch(err=>{
-                console.log(err);
-                res.json({error:err});
-            })
+            }
+            else{
+                sql = "update info set state = " + req.body.state
+                            + " where id = " + req.body.id;
+            }
+            return conn.query(sql);
+        })
+        .then(rows=>{
+            res.json({success:"success"});
+        })          
+        .catch(err=>{
+            console.log("updatestate:",err);
+            res.json({error:"unknown error"});
+        });
     });
 
     // update ddl
@@ -222,21 +267,40 @@ app.use(cors());
             return;
         }
         var id = req.body.id;
-        var ddl = req.body.ddl;
-
-        var queryUpdate = "update info set ddl = \'" + ddl + "\' where id = " + id;
-
-        conn.query(queryUpdate)
+        var sql = "select * from info where id = " + id;
+        conn.query(sql)
         .then(rows=>{
-            var queryInsert = "insert into ddl (id,ddl) values (" + id + ",\'" + ddl + "\')";
-            return conn.query(queryInsert);
+            if( rows.length < 1 ){
+                res.json({error:"invalid id "+id});
+                return;
+            }
+            if( !isActivated(rows[0].state) ){
+                res.json({error:"target " + id + " state is " + rows[0].state});
+                return;
+            }
+            var newDdl = new Date(req.body.ddl);
+            if( newDdl <= Date.now() ){
+                res.json({error:"cannot set a ddl earlier than the current time " + Date.now()});
+                return;
+            } 
+            
+            sql = "update info set ddl = \'" + ddl + "\' where id = " + id;
+            return conn.query(sql);
         })
         .then(rows=>{
-            res.json({success:"success"});
+            sql = "insert into ddl (id,ddl) values (" + id + ",\'" + req.body.ddl + "\')";
+            return conn.query(sql);
+        })          
+        .then(rows=>{
+            var info = {id:id,ddl:req.body.ddl,state:"PENDING"};
+            updateNotifyJob(info,param);
+            updateAutoDelayJob(info,param);
         })
         .catch(err=>{
-            res.json({error:err});
-        })
+            console.log("updateddl:",err);
+            res.json({error:"unknown error"});
+        });
+
     });
 
     // update progress
@@ -266,296 +330,3 @@ app.use(cors());
     app.listen(port,()=>{console.log("listening at http://localhost:${port}")});
     
 })();
-/*
-mariadb.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'okrtabledb'
-})
-.then(conn=>{
-
-    // post param: level,start,duration
-    // start : yyyy-mm-dd
-    // duration : n days
-    // level: 0 or 1 optional
-    // priority: 0,1,2 optional
-    // state: "PENDING","INPROG","DONE","BLOCKED","KILLED" optional
-    // select all object whose ddl date in range.
-    // range from yyyy-mm-dd 00:00:00 to n*24 hours later
-    // n must >= 0 , if n == 0 , only select the object whose ddl is at yyyy-mm-dd 00:00:00
-    app.post('/',checkRequest,(req,res)=>{
-        test(req,res,conn);
-        if( !("start" in req.body) ){
-            res.json({error:"no start parameter"});
-            return;
-        }
-        if( !("duration" in req.body) ){
-            res.json({error:"no duration parameter"});
-            return;
-        } 
-        
-        var startDate = new Date(req.body.start + " 00:00:00");
-        var endDate = new Date(req.body.start + " 00:00:00");
-        endDate.setDate(startDate.getDate() + parseInt(req.body.duration));
-        var startDateStr = date2str(startDate);  
-        var endDateStr = date2str(endDate);
-
-        var queryRange = "select * from info where ddl >= \'" + 
-                    startDateStr + "\' and ddl <= \'" + endDateStr + "\' ";
-        if( "level" in req.body ){
-            queryRange += "and level = " + req.body.level;
-        }
-        conn.query({dateStrings:true, sql:queryRange})
-        .then( (rows)=>{
-            var curDate = new Date(Date.now());
-            var result = [];
-            for(var i=0;i<rows.length;++i){
-                var ddlDate = new Date(rows[i].ddl);
-                if( (rows[i].state == 'PENDING' || rows[i].state == 'INPROG') && ddlDate < curDate ){
-                    console.log("timeout");
-                    var diffDay = Math.floor((curDate - ddlDate) / 86400000); // 24*3600*1000
-                    var step = rows[i].level == 0 ? 30 : 7;
-                    var diff = Math.floor(diffDay/step) + 1;
-                    console.log(diffDay,step,diff);
-                    rows[i].priority -= diff;
-                    if( rows[i].priority < 0 ){
-                        rows[i].priority = 0;
-                        rows[i].state = 'KILLED';
-                        var queryUpdate = "update info set state = \'KILLED\',priority=0" + " where id = " + rows[i].id;
-                        console.log(queryUpdate);
-                        //conn.query(queryUpdate);
-                    }
-                    else{
-                        var ddlStr = date2str(new Date(ddlDate.setDate(ddlDate.getDate() + (step*diff) )));
-                        rows[i].ddl = ddlStr;
-                        var queryUpdateInfo = "update info set priority = " + rows[i].priority + ", ddl = \'" + ddlStr + "\' where id = " + rows[i].id;
-                        var queryInsertDdl = "insert into ddl values (" + rows[i].id + ",\'" + ddlStr + "\')";
-                        console.log(queryUpdateInfo);
-                        console.log(queryInsertDdl);
-                        //conn.query(queryUpdateInfo);
-                        //conn.query(queryInsertDdl);
-                    }
-                }
-                var flag = true;
-                if( "priority " in req.body && req.body.priority != rows[i].priority ){
-                    flag = false;
-                }
-                if( "state" in req.body && req.body.state != rows[i].state ){
-                    flag = false;
-                }
-                if( flag ){
-                    result.push(rows[i]);
-                }
-            }
-            res.send(result);
-        })
-        .catch(err=>{
-            console.log("Error: " + err);
-            res.json({error:"query error"});
-        })
-
-    });
-
-    // get progress by id
-    // id: yyyymmddnn
-    app.post('/getprogress',checkRequest,(req,res)=>{
-        if( !("id" in req.body) ){
-            res.json({error:"no id parameter"});
-            return;
-        }
-        var querySql = "select * from progress where id = " + req.body.id;
-        conn.query(querySql)
-        .then((rows)=>{
-            res.send(rows);
-        })
-        .catch(err=>{
-            res.json({error:err});
-        })
-    })
-
-    // create new record
-    // detail: String
-    // level: 0 or 1
-    // priority: 0,1,2
-    // state: "PENDING","INPROG","DONE","BLOCKED","KILLED"
-    // ddl: yyyy-mm-dd hh:mm:ss
-    app.post('/create',checkRequest,(req,res)=>{
-        // todo check detail content for security
-        if( !("detail" in req.body) ){
-            res.json({error:"no detail parameter"});
-            return;
-        }
-        var detail = req.body.detail;
-
-        if( !("level" in req.body) ){
-            res.json({error:"no level parameter"});
-            return;
-        }
-        var level = req.body.level;
-
-        if( "priority" in req.body ){
-            var priority = req.body.priority;
-        }
-        else{
-            var priority = 2;
-        }
-
-        if( "state" in req.body ){
-            var state = req.body.state;
-        }
-        else{
-            var state = "PENDING";
-        }
-
-        if( !("ddl" in req.body) ){
-            res.json({error:"no ddl parameter"});
-            return;
-        }
-        var ddl = req.body.ddl;
-
-        var today = new Date(Date.now());
-        var start = today.getFullYear() * 10000 + (today.getMonth()+1)*100 + today.getDate();
-        start *= 100;
-        var end = start + 99;
-        var queryCount = "select count(*) as count from info where id >= " + start + " and id <= " + end;
-        var id = start;
-
-        conn.query(queryCount)
-            .then(rows=>{
-                id = start + rows[0].count;
-                var queryInsert = "insert into info values (" + id + ",\'" + detail + "\'," + level
-                                    + "," + priority + ",\'" + state + "\',\'" + ddl + "\')";
-                return conn.query(queryInsert);    
-            })
-            .then(rows=>{
-                var queryInsert = "insert into ddl (id,ddl) values (" + id + ",\'" + ddl + "\')";
-                return conn.query(queryInsert); 
-            })
-            .then(rows=>{
-                res.json({success:id});
-            })
-            .catch(err=>{
-                console.log("Error: " + err);
-                res.json({error: err});
-            })
-    });
-
-    // update priority
-    // id: yyyymmddnn
-    // priority: 0,1,2
-    // todo check query result
-    app.post('/updateprior',checkRequest,(req,res)=>{
-        if( !("id" in req.body) ){
-            res.json({error:"no id parameter"});
-            return;
-        }
-        if( !("priority" in req.body) ){
-            res.json({error:"no priority parameter"});
-            return;
-        }
-        
-        var queryUpdate = "update info set priority = " + req.body.priority
-                            + " where id = " + req.body.id;
-        
-        conn.query(queryUpdate)
-            .then(rows=>{
-                res.json({success:"success"});
-            })
-            .catch(err=>{
-                console.log(err);
-                res.json({error:err});
-            })
-    });
-
-    // todo use middleware to check date format
-    // update state
-    // id: yyyymmddnn
-    // state: "PENDING","INPROG","DONE","BLOCKED","KILLED"
-    app.post('/updatestate',checkRequest,(req,res)=>{
-        if( !("id" in req.body) ){
-            res.json({error:"no id parameter"});
-            return;
-        }    
-        if( !("state" in req.body) ){
-            res.json({error:"no state parameter"});
-            return;
-        }
-
-        var queryUpdate = "update info set state = \'" + req.body.state 
-                            + "\' where id = " + req.body.id;
-        
-        conn.query(queryUpdate)
-            .then(rows=>{
-                res.json({success:"success"});
-            })
-            .catch(err=>{
-                console.log(err);
-                res.json({error:err});
-            })
-    });
-
-    // update ddl
-    // id: yyyymmddnn
-    // ddl: 'yyyy-mm-dd hh:mm:ss'
-    // todo compare ddl with current time and change state
-    app.post('/updateddl',checkRequest,(req,res)=>{
-        if( !("id" in req.body) ){
-            res.json({error:"no id parameter"});
-            return;
-        }
-        if( !("ddl" in req.body) ){
-            res.json({error:"no ddl parameter"});
-            return;
-        }
-        var id = req.body.id;
-        var ddl = req.body.ddl;
-
-        var queryUpdate = "update info set ddl = \'" + ddl + "\' where id = " + id;
-
-        conn.query(queryUpdate)
-        .then(rows=>{
-            var queryInsert = "insert into ddl (id,ddl) values (" + id + ",\'" + ddl + "\')";
-            return conn.query(queryInsert);
-        })
-        .then(rows=>{
-            res.json({success:"success"});
-        })
-        .catch(err=>{
-            res.json({error:err});
-        })
-        
-    });
-
-    // update progress
-    // id: yyyymmddnn
-    // progress: String
-    app.post('/updateprogress',checkRequest,(req,res)=>{
-        if( !("id" in req.body) ){
-            res.json({error:"no id parameter"});
-            return;
-        }
-        if( !("progress" in req.body) ){
-            res.json({error:"no progress parameter"});
-            return;
-        }
-        var id = req.body.id;
-        var progress = req.body.progress;
-        var queryUpdate = "insert into progress (id,progress) values (" + id + ",\'" + progress + "\')";
-        conn.query(queryUpdate)
-        .then(rows=>{
-            res.json({success:"success"});
-        })
-        .catch(err=>{
-            res.json({error:err});
-        });
-    });
-
-    app.listen(port,()=>{console.log("listening at http://localhost:${port}")});
-})
-.catch(err=>{
-    console.log(err);
-});
-*/
-
-
--- utils.js
+++ utils.js
@@ -1,4 +1,7 @@
-function date2str(date){
+const schedule = require('node-schedule');
+
+function date2str(date)
+{
     return date.getFullYear().toString().padStart(4,"0") + "-" + 
             (date.getMonth()+1).toString().padStart(2,"0") + "-" +
               date.getDate().toString().padStart(2,"0") + " " + 
@@ -6,5 +9,137 @@ function date2str(date){
                   date.getMinutes().toString().padStart(2,"0") + ":" +
                     date.getSeconds().toString().padStart(2,"0"); 
 }
+function addJobs(id,date,jobs,callbackfun)
+{
+    if( id in jobs){
+        jobs[id].reschedule(date);
+    }
+    else{
+        jobs[id] = schedule.scheduleJob(date,callbackfun);
+    }
+}
+function isActivated(state)
+{
+    return state == "PENDING" || state == "INPROG";
+}
+
+async function initJobs(param)
+{
+    try{
+        var rows = await param.conn.query("select * from info where state = \'PENDING\' or state = \'INPROG\'");
+    }
+    catch(err){
+        console.log("Init jobs error:",err);
+        return;
+    }
+
+    for(var i=0;i<rows.length;++i){
+        await updateStateHandler(rows[i],param.conn);
+        updateNotifyJob(rows[i],param);
+        updateAutoDelayJob(rows[i],param);
+    }
+}
+function updateNotifyJob(info,param)
+{
+    if( !isActivated(info.state) ){
+        if( info.id in param.notifyJobs ){
+            param.notifyJobs[info.id].cancel();
+            delete param.notifyJobs[info.id];
+        }
+        return;
+    }
+    var notifyDate = new Date(info.ddl);
+    notifyDate.setDate(notifyDate.getDate() - (info.level == 0 ? 7:1));
+    if(notifyDate <= Date.now()){
+        notifyDate = new Date(Date.now());
+        notifyDate.setSeconds(notifyDate.getSeconds() + 10); 
+    } 
+    console.log("add notify ", notifyDate,info.level,info.id);
+    addJobs(info.id,notifyDate,param.notifyJobs,notify.bind(null,info.id,param));
+}
+async function notify(id,param)
+{
+    try{
+        var rows = await param.conn.query("select * from info where id = " + id);
+        console.log("notification",rows[0].detail,rows[0].ddl);
+    }
+    catch(err){
+        console.log("notify:",err);
+    }
+    delete param.notifyJobs[id];
+}
+function updateAutoDelayJob(info,param)
+{
+    if( !isActivated(info.state) ){
+        if( info.id in param.autoDelayJobs ){
+            param.autoDelayJobs[info.id].cancel();
+            delete param.autoDelayJobs[info.id];
+        }
+        return;
+    }
+    var autoDelayDate = new Date(info.ddl);
+    if( autoDelayDate <= Date.now()){
+        autoDelayDate = new Date(Date.now());
+        autoDelayDate.setSeconds(notifyDate.getSeconds() + 10); 
+    }
+    addJobs(info.id,autoDelayDate,param.autoDelayJobs,autoDelay.bind(null,info.id,param));
+}
+async function autoDelay(id,param)
+{
+    try{
+        var rows = await param.conn.query("select * from info where id = "+id);
+        console.log("autodelay:",rows[0].id);
+    }
+    catch(err){
+        console.log("autodelay:",err);
+    }
+    await updateStateHandler(row,param.conn); 
+    updateNotifyJob(rows[0],param);
+    updateAutoDelayJob(rows[0],param);
+}
+async function updateStateHandler(info,conn)
+{
+    var ddlDate = new Date(info.ddl);
+    var currentDate = new Date(Date.now());
+    if( ddlDate > currentDate ){
+        return;
+    }
+    var diffDay = Math.floor((currentDate - ddlDate) / 86400000); // 24*3600*1000
+    var step = info.level == 0 ? 30 : 7;
+    var diff = Math.floor(diffDay/step) + 1;
+    info.priority -= diff;
+    if( info.priority < 0 ){
+        info.priority = 0;
+        info.state = "KILLED";
+        var queryUpdate = "update info set state = \'KILLED\',priority=0,ddl=\'"
+                            + date2str(new Date(Date.now()))+ "\'where id = " + info.id;
+        try{
+            await conn.query(queryUpdate);
+        }
+        catch(err){
+            console.log("update state error",err);
+        }
+        return;
+    }
+
+    ddlDate = new Date(ddlDate.setDate(ddlDate.getDate() + (step*diff) ));
+    var ddlStr = date2str(ddlDate);
+    info.ddl = ddlStr;
+    var queryUpdateInfo = "update info set priority = " + info.priority + ", ddl = \'" + ddlStr + "\' where id = " + info.id;
+    var queryInsertDdl = "insert into ddl (id,ddl) values (" + info.id + ",\'" + ddlStr + "\')";
+    console.log(queryUpdateInfo);
+    console.log(queryInsertDdl);
+    try{
+        await conn.query(queryUpdateInfo);
+        await conn.query(queryInsertDdl);
+    }
+    catch(err){
+        console.log("update state error",err);
+    }
+}
 
 module.exports.date2str = date2str;
+module.exports.isActivated = isActivated;
+module.exports.initJobs = initJobs;
+module.exports.updateNotifyJob = updateNotifyJob;
+module.exports.updateAutoDelayJob = updateAutoDelayJob;
